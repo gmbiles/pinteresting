9a160e4a50d0ec322057b5fdab0931458ffc49cc
gmbiles <grant@gradible.com>
2015-09-29 17:25:23 -0400

    Add Amazon S3 for Paperclip uploads to Haroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,15 @@ Rails.application.configure do
     # required for Heroku
     #Not to set this to your actual host
   config.action_mailer.default_url_options = { host: 'https://gradible-pslf.herokuapp.com/' }
+
+  # Sets paperclip to upload images to Amazon S3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+
 end
